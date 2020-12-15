d38a4f07c3883ba0bb773745966a9886ab63de41
sperezconesa <sperezconesa@gmail.com>
2020-12-15 17:26:18 +0100

    First correctly trained model

--- src/data/get_external_data.py
+++ src/data/get_external_data.py
@@ -1,5 +1,5 @@
 from fetch_data import gsheet_to_csv
 from fetch_data import gsheet_to_csv,gsheet_to_csv_url
 external_data_dir = './data/external/'
-for i in ['por1', 'por2', 'por3', 'por4', 'non1', 'non2', 'non3', 'non4']:
+for i in ['APM1', 'APM2', 'APM3', 'APM4', 'BPM1', 'BPM2', 'BPM3', 'BPM4']:
     gsheet_to_csv_url('https://docs.google.com/spreadsheets/d/1zBEQ2AH5vEv1mTwgjwWeu0TDon-cZyZODDN_BCjWpCk/edit#gid=0', f'{i}', external_data_dir+f'{i}.csv')
